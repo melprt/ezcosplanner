9e749b3b715723c8d51393e5c342c86390be5f3e
Melissa POIROT 🐱 <melissa.toriop@gmail.com>
2024-09-30 18:16:10 +0200

    feat(cosplan-part-edit): wip view
    Wip picture card + display in full screen with description => done

--- apps/ezcosplanner/src/app/components/dashboard/parts/wip-picture/card/wip-picture-card.component.ts
+++ apps/ezcosplanner/src/app/components/dashboard/parts/wip-picture/card/wip-picture-card.component.ts
@@ -1,12 +1,19 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatIconButton } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'ezc-wip-picture-card',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, MatIconModule, MatIconButton],
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    MatIconModule,
+    MatIconButton,
+    MatIcon,
+    MatButtonModule,
+  ],
   templateUrl: './wip-picture-card.component.html',
   styleUrl: './wip-picture-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,7 +21,7 @@ import { MatIconButton } from '@angular/material/button';
 export class WipPictureCardComponent {
   expandedPicture = signal<boolean>(false);
 
-  togglePictureFullSize () : void {
-    this.expandedPicture.update(val => !val);
+  togglePictureFullSize(): void {
+    this.expandedPicture.update((val) => !val);
   }
 }
